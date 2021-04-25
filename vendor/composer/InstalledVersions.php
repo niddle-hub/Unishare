3382065fa27dcc09f0383344b192def94b60a61f
niddle <haus2013@yandex.ru>
2021-04-26 02:02:34 +0500

    update with composer

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -30,20 +30,11 @@ private static $installed = array (
     'aliases' => 
     array (
     ),
-    'reference' => '328ff3476caf2b98d9712c473ce15ebe120f7437',
-    'name' => '__root__',
+    'reference' => '64e4dd24dfbc241b952940178d6ea63a49d6ecd4',
+    'name' => 'niddle/unishare',
   ),
   'versions' => 
   array (
-    '__root__' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '328ff3476caf2b98d9712c473ce15ebe120f7437',
-    ),
     'digitalstars/simplevk' => 
     array (
       'pretty_version' => 'v2.14.0',
@@ -61,6 +52,25 @@ private static $installed = array (
       array (
       ),
       'reference' => '7fc9cc1f519e5b2342c3ce00d98e53ed32b33011',
+    ),
+    'niddle/unishare' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '64e4dd24dfbc241b952940178d6ea63a49d6ecd4',
+    ),
+    'roave/security-advisories' => 
+    array (
+      'pretty_version' => 'dev-latest',
+      'version' => 'dev-latest',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '3c97c13698c448fdbbda20acb871884a2d8f45b1',
     ),
   ),
 );
